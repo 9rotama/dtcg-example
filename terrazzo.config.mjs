9b9baf0aed974ce600944da814cd147ab4d911f9
9rotama <aoim99.sc@gmail.com>
2025-12-06 21:43:26 +0900

    effect(shadows)

--- terrazzo.config.mjs
+++ terrazzo.config.mjs
@@ -5,16 +5,17 @@ export default defineConfig({
   tokens: ['./tokens/digital-go.tokens.json'],
   plugins: [
     css({
-      filename: "css-tokens.css"
+      filename: 'css-tokens.css'
     }),
     tailwind({
-      filename: "tailwind-tokens.css",
+      filename: 'tailwind-tokens.css',
       theme: {
         /** @see https://tailwindcss.com/docs/configuration#theme */
         colors: ['Color.Color.*'],
         text: ['Typography.FontSize.*'],
         font: ['Typography.FontFamily.*'],
-        radius: ['Size.BorderRadius.*']
+        radius: ['Size.BorderRadius.*'],
+        shadow: ['Effect-styles.Elevation.*']
       }
     })
   ],
